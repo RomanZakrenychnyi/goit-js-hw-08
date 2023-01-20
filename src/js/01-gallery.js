5c86f7d41776e825393028c1ddb64ec34f6d3eba
RomanZakrenychnyi <116659319+RomanZakrenychnyi@users.noreply.github.com>
2023-01-20 15:46:55 +0200

    hw-done!

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,28 @@
-// Add imports above this line
 import { galleryItems } from './gallery-items';
-// Change code below this line
+import SimpleLightbox from "simplelightbox";
+import "simplelightbox/dist/simple-lightbox.min.css";
 
-console.log(galleryItems);
+
+//                  ADD GALLERY
+
+const galleryRef = document.querySelector('.gallery');
+const cardGallery = galleryItems
+  .map(({ preview, original, description }) => {
+    return `<a class="gallery__item" href=${original}>
+            <img class="gallery__image"
+            src=${preview} 
+            alt=${description} />
+            </a>`;
+  })
+  .join('');
+
+galleryRef.insertAdjacentHTML('afterbegin', cardGallery);
+
+//                  ADD SimpleLightbox
+
+new SimpleLightbox('.gallery .gallery__item', {
+    captionsData: 'alt',
+    captionPosition: 'bottom',
+    captionDelay: 250
+})
+
--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,35 @@
+import throttle from "lodash.throttle" 
+
+const form = document.querySelector('.feedback-form')
+const email = document.querySelector('input')
+const textarea = document.querySelector('textarea')
+
+form.addEventListener('submit', onFormSubmit)
+form.addEventListener('input', throttle(onFormInput, 500))
+
+populateTextarea();
+
+function onFormInput() {
+    const userDate = {
+        email: email.value,
+        text: textarea.value,
+    }
+    localStorage.setItem('feedback-form-state', JSON.stringify(userDate))
+}
+
+function onFormSubmit(event) {
+    event.preventDefault();
+    localStorage.removeItem('feedback-form-state');
+    event.target.reset()
+}
+
+function populateTextarea() {
+    const userDateObj = JSON.parse(localStorage.getItem('feedback-form-state'))
+    if (userDateObj) {
+        email.value = userDateObj.email
+        textarea.value = userDateObj.text
+        console.log(userDateObj)
+    }
+}
+
 
