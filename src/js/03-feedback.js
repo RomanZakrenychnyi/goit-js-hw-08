4fb44e146e61ac25c4a2974f2c6f75d2384ca12a
RomanZakrenychnyi <116659319+RomanZakrenychnyi@users.noreply.github.com>
2023-02-19 10:11:16 +0200

    task-03 done! got some troble with terminal, so i did it in GoLive

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,80 +1,53 @@
-// const formEl = document.querySelector('.feedback-form');
-// console.log('formEl');
-// const handleFormSubmit = event => {
-//     event.preventDefault()
-//     console.log(event.target)
-// }
+// import throttle from "./lodash.js" 
 
-// formEl.addEventListener('submit', handleFormSubmit);
+const formEl = document.querySelector('.feedback-form');
+const emailEl = document.querySelector('input[name="email"]');
+const textAreaEl = document.querySelector('textarea[name="message"]');
 
 
+const checkedLs = () => {
+    if (localStorage.getItem('feedback-form-state') || localStorage.getItem('feedback-form-state') !== null) {
+        const parsedLs = JSON.parse(localStorage.getItem('feedback-form-state'));
+        const { email, message } = parsedLs;
+        emailEl.value = email;
+        textAreaEl.value = message;
+    }
+}
 
-// console.log('dsadasd');
-
-
-
-
-
-
-
-
-
-
+checkedLs()
 
 
+const handleFormSubmit = (event) => {
+    event.preventDefault();
+
+    const { email, message } = event.target.elements;
+    const mail = email.value;
+    const text = message.value;
+
+    const userData = {
+        email: mail,
+        message: text,
+    }
+
+    console.log(userData);
+    event.target.reset();
+    localStorage.removeItem('feedback-form-state');
+}
 
 
+const handleInputLs = ({ target }) => {
+    console.dir(target);
+    const userData = {
+        email: target.form[0].value,
+        message: target.form[1].value,
+    }
 
+    const jsonUserData = JSON.stringify(userData)
 
+    localStorage.setItem('feedback-form-state', jsonUserData);
+}
 
+// const throttledFunction = throttle(handleInputLs, 500);
 
-
-
-
-
-
-
-
-
-
-
-
-// import throttle from "lodash.throttle" 
-
-// const form = document.querySelector('.feedback-form')
-// const email = document.querySelector('input')
-// const textarea = document.querySelector('textarea')
-
-// form.addEventListener('submit', onFormSubmit)
-// form.addEventListener('input', throttle(onFormInput, 500))
-
-// populateTextarea();
-
-// const userDate = {
-//         email: '',
-//         text: '',
-//     }
-
-// function onFormInput() {
-//     userDate.email = email.value;
-//     userDate.text = textarea.value;
-//     localStorage.setItem('feedback-form-state', JSON.stringify(userDate))
-// }
-
-// function onFormSubmit(event) {
-//     event.preventDefault();
-//     localStorage.removeItem('feedback-form-state');
-//     event.target.reset()
-//     console.log(userDate)
-// }
-
-// function populateTextarea() {
-//     const userDateObj = JSON.parse(localStorage.getItem('feedback-form-state'))
-//     if (userDateObj) {
-//         email.value = userDateObj.email
-//         textarea.value = userDateObj.text
-//         console.log(userDateObj)
-//     }
-// }
-
-
+formEl.addEventListener('submit', handleFormSubmit);
+formEl.addEventListener('input', handleInputLs);
