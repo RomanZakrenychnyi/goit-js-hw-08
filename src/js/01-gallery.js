4f7a7f61916ba65c43e27bef65bafeeccc1b444b
RomanZakrenychnyi <116659319+RomanZakrenychnyi@users.noreply.github.com>
2023-02-27 11:49:34 +0200

    updates

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,6 +1,6 @@
-import { galleryItems } from './gallery-items';
-import "simplelightbox/dist/simple-lightbox.min.css";
 import SimpleLightbox from "simplelightbox";
+import "simplelightbox/dist/simple-lightbox.min.css";
+import { galleryItems } from './gallery-items';
 
 const galleryRef = document.querySelector('.gallery');
 const cardGallery = galleryItems
