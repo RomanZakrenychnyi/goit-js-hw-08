5214ddb2b2c4b013c0ea307bb8edcfbc3f742ea2
RomanZakrenychnyi <116659319+RomanZakrenychnyi@users.noreply.github.com>
2023-02-27 12:09:19 +0200

    Прибрав зайву перевірку. Добавив троттл на колбєк слухача инпуту. Ключ сховища виніс в окрему змінну.

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,13 +1,15 @@
-// import throttle from "./lodash.js" 
+const throttle = require('lodash.throttle');
 
 const formEl = document.querySelector('.feedback-form');
 const emailEl = document.querySelector('input[name="email"]');
 const textAreaEl = document.querySelector('textarea[name="message"]');
 
+const LS_KEY = 'feedback-form-state'
 
-const checkedLs = form => {
-    if (localStorage.getItem('feedback-form-state') || localStorage.getItem('feedback-form-state') !== null) {
-        const parsedLs = JSON.parse(localStorage.getItem('feedback-form-state'));
+
+const checkedLs = () => {
+    if (localStorage.getItem(LS_KEY)) {
+        const parsedLs = JSON.parse(localStorage.getItem(LS_KEY));
         const { email, message } = parsedLs;
         emailEl.value = email;
         textAreaEl.value = message;
@@ -31,12 +33,11 @@ const handleFormSubmit = (event) => {
 
     console.log(userData);
     event.target.reset();
-    localStorage.removeItem('feedback-form-state');
+    localStorage.removeItem(LS_KEY);
 }
 
 
 const handleInputLs = ({ target }) => {
-    console.log(target.value);
     const userData = {
         email: target.form[0].value,
         message: target.form[1].value,
@@ -44,10 +45,10 @@ const handleInputLs = ({ target }) => {
 
     const jsonUserData = JSON.stringify(userData)
 
-    localStorage.setItem('feedback-form-state', jsonUserData);
+    localStorage.setItem(LS_KEY, jsonUserData);
 }
 
-// const throttledFunction = throttle(handleInputLs, 500);
+const throttledFunction = throttle(handleInputLs, 500);
 
 formEl.addEventListener('submit', handleFormSubmit);
-formEl.addEventListener('input', handleInputLs);
+formEl.addEventListener('input', throttledFunction);
