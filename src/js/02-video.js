a7748f6fa6b68b5e3b1c67a7c22fcd401fc0f6b4
RomanZakrenychnyi <116659319+RomanZakrenychnyi@users.noreply.github.com>
2023-02-27 12:54:35 +0200

    add to local storage 1 parametr = 0

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,15 +1,17 @@
 import throttle from 'lodash.throttle'
 
+const LS_KEY = "videoplayer-current-time"
+
 const iframe = document.querySelector('iframe');
 const player = new Vimeo.Player(iframe);
 
 
 const onPlay = function ({ seconds }) {
-    localStorage.setItem("videoplayer-current-time", seconds)
+    localStorage.setItem(LS_KEY, seconds)
 };
 
 const throttledOnPlay = throttle(onPlay, 1000)
 
 player.on('timeupdate', throttledOnPlay);
 
-player.setCurrentTime(localStorage.getItem("videoplayer-current-time"));
+player.setCurrentTime(localStorage.getItem(LS_KEY) || 0);
